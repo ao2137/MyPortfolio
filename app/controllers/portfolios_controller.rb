abc6bec8a5567ec1d08a7a68bb8dcd398730824b
ao2137 <allison.oldfather@gmail.com>
2017-07-13 06:52:33 -0400

    added edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,4 +21,28 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+     @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update 
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to @portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
 end
