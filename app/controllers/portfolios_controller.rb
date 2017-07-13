764032b03bd0a6ce6c482996943fbb3823380f45
ao2137 <allison.oldfather@gmail.com>
2017-07-13 07:36:55 -0400

    implemented show for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,37 +12,31 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
-
       end
     end
   end
 
   def edit
-     @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
-  def update 
+  def update
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
 
-
-  def destroy
-    @portfolio_item.destroy
-    respond_to do |format|
-      format.html { redirect_to @portfolios_path, notice: 'Portfolio was successfully destroyed.' }
-    end
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
